21d590e9dd4cad35845c79343829919d46120a79
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-11-18 14:27:43 +0530

    ft: form submission code

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,7 +1,26 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
+test_image_url = "https://media.npr.org/assets/img/2021/03/05/nyancat-still_custom-151b372a60f244f026ea3ca46a3530609e57fceb.png"
+
+nfts = [
+    {
+        "title": "Elephant NFT",
+        "image": test_image_url,
+        "cost": 100,
+    },
+    {
+        "title": "Penguin NFT",
+        "image": test_image_url,
+        "cost": 200,
+    },
+    {
+        "title": "Zebra NFT",
+        "image": test_image_url,
+        "cost": 300,
+    },
+]
 
 
 @app.route("/")
@@ -12,4 +31,19 @@ def root():
 @app.route("/hello/")
 @app.route("/hello/<name>")
 def hello(name=None):
-    return render_template("index.html", name=name)
+    return render_template("index.html", name=name, nfts=nfts)
+
+
+# Handle forms and form submissions
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    if request.method == "GET":
+        return render_template("form.html")
+    elif request.method == "POST":
+        data = request.form
+        return render_template("output.html", data=data)
+
+
+if __name__ == 'main':
+    app.run(debug=True)
+
